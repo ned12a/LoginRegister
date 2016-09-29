b6f94ffd4b89c1f4f15cdd795d71ae4f3e3647b0
ned12a <ned12a@users.noreply.github.com>
2016-09-29 09:04:31 -0700

    Update Register.php

--- Register.php
+++ Register.php
@@ -2,12 +2,13 @@
     $con=mysqli_connect("localhost","my_user","my_password","my_db");
     
     $name = $_POST["name"];
-    $age = $_POST["age"];
     $username = $_POST["username"];
+    $email = $_POST["email"];
     $password = $_POST["password"];
+    $ContactNo = $_POST["ContactNo"];
     
-    $statement = mysqli_prepare($con, "INSERT INTO User (name, age, username, password) VALUES (?, ?, ?, ?)");
-    mysqli_stmt_bind_param($statement, "siss", $name, $age, $username, $password);
+    $statement = mysqli_prepare($con, "INSERT INTO User (name, username, email, password, ContactNo) VALUES (?,?, ?, ?, ?)");
+    mysqli_stmt_bind_param($statement, "ssssi", $name, $username, $password ,$ContactNo);
     mysqli_stmt_execute($statement);
     
     mysqli_stmt_close($statement);
